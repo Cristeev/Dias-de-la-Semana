ea49ec51e012c38cbc25926b801d1379c7dd9147
Cristeev <cristopherflamenco@hotmail.com>
2015-01-28 18:35:26 -0600

    tarea

---
+++ diasdelasemana.js
@@ -0,0 +1,42 @@
+
+
+function dia(valor){
+	console.log(valor);
+
+	switch (5){
+
+		case 1: 
+			console.log("lunes");
+		break;
+
+		case 2:
+			console.log("martes");
+		break;
+
+		case 3:
+			console.log("miercoles");
+		break;
+
+		case 4:
+			console.log("jueves");
+		break;
+
+		case 5:
+			console.log("viernes");
+		break;
+
+		case 6: 
+			console.log("sábado");
+		break;
+
+		case 7:
+			console.log("domingo");
+		break;
+
+		default:
+			console.log("valores válidos: del 1 al 7");
+
+	}
+}
+
+dia(dia); 
